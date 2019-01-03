a85278c4084a10da00c9c29893dc4be9614f5701
Ryan Ren <yueren@microsoft.com>
2019-01-03 15:24:29 -0800

    Added deepDive1.ts

---
+++ bin/deepDive1.js
@@ -0,0 +1,23 @@
+//#region NaN equality checks
+// Don't do this
+console.log(NaN === NaN); // false!!
+// Do this
+console.log(Number.isNaN(NaN)); // true
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+console.log(-Number.MAX_VALUE); // -1.7976931348623157e+308
+//#endregion
+//#region Classes
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    add(point) {
+        return new Point(this.x + point.x, this.y + point.y);
+    }
+}
+let p1 = new Point(0, 10);
+let p2 = new Point(10, 20);
+let p3 = p1.add(p2); // {x:10,y:30}
+//#endregion
+//# sourceMappingURL=deepDive1.js.map
